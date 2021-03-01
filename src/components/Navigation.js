10acd9f2025e26debd32bf7368a60ce7b9f42e22
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-03-01 13:25:43 +0100

    Fix links to github and linkedin problems

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
 // import '../stylesheets/App.css';
-import IconNavigation from './IconNavigation';
+import IconNavigation from "./IconNavigation";
 
 class Navigation extends React.Component {
-  /*   constructor(props) {
-    super(props);
-  } */
+  linkToGithub(githubUser) {
+    let githubValue = githubUser;
+    let newGithubValue = githubValue.replace("https://github.com/", "");
+    let previewGithub;
+    if (githubValue === "") {
+      previewGithub = "#";
+    } else {
+      previewGithub = `https://github.com/${newGithubValue}`;
+    }
+    return previewGithub;
+  }
+  linkToLinkedin(linkedinUser) {
+    let linkedinValue = linkedinUser;
+    let newLinkedinValue = linkedinValue.replace(
+      "https://www.linkedin.com/in/",
+      ""
+    );
+    let previewLinkedin;
+    if (linkedinValue === "") {
+      previewLinkedin = "#";
+    } else {
+      previewLinkedin = `https://www.linkedin.com/in/${newLinkedinValue}`;
+    }
+    return previewLinkedin;
+  }
 
   render() {
     return (
@@ -21,19 +43,19 @@ class Navigation extends React.Component {
             numberPalette={this.props.numberPalette}
             jsClass="email"
             icon="far fa-envelope"
-            href={this.props.email}
+            href={"mailto:" + this.props.email}
           />
           <IconNavigation
             numberPalette={this.props.numberPalette}
             jsClass="linkedin"
             icon="fab fa-linkedin-in"
-            href={this.props.linkedin}
+            href={this.linkToLinkedin(this.props.linkedin)}
           />
           <IconNavigation
             numberPalette={this.props.numberPalette}
             jsClass="github"
             icon="fab fa-github-alt"
-            href={this.props.github}
+            href={this.linkToGithub(this.props.github)}
           />
         </ul>
       </nav>
