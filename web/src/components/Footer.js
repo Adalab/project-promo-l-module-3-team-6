7f26d1a407b7df02ef53385f7f210f2a2f39b584
EstLopCor <estherlopezcordoncillo@gmail.com>
2021-04-21 15:16:06 +0200

    update footer

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import '../stylesheets/layout/_footer.scss';
-import logo from '../images/logo-adalab.png';
+import React from "react";
+import "../stylesheets/layout/_footer.scss";
+import logo from "../images/logo-adalab.png";
 
 class Footer extends React.Component {
   /*   constructor(props) {
@@ -11,7 +11,7 @@ class Footer extends React.Component {
     return (
       <footer className="footer">
         <div className="footer__container">
-          <small className="footer__copy">Tree huggers cards &copy; 2021</small>
+          <small className="footer__copy">Tree Huggers Cards &copy; 2021</small>
           <a href="https://adalab.es/" target="_blank" alt="">
             <img
               className="footer__logo"
